cf9060d93ec50cce37bc8b4d6e90ba015de39fa0
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-30 05:15:11 +0700

    Introduction Frontend Desa

---
+++ README.md
@@ -0,0 +1,3 @@
+## Front End Village Information System 
+
+<h2>fe-desa</h2>
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import { Toaster } from 'react-hot-toast'; // import Toast
+
+function App() {
+  return (
+    <>
+      <Toaster />
+    </>
+  )
+}
+
+export default App
