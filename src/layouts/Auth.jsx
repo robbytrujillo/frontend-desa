80892cf98039ebffe5f5ad7373ae039487ee76b9
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-30 05:56:11 +0700

    Introduction Frontend Desa | Route Login Configure

---
+++ src/layouts/Auth.jsx
@@ -0,0 +1,26 @@
+// import css bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/admin/css/custom.css";
+
+// import font awesome
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+export default function auth({ children }) {
+    return (
+        <div 
+            style={{ 
+                backgroundImage: "url(/images/bg.png)",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                height: "100vh",
+             }}
+        >
+            <div className="container">
+                <div className="d-flex justify-content-center h-100">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
