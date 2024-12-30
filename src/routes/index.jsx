64b83827278295f0f7717db61e69e65cb824667d
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-30 13:22:03 +0700

    Introduction Frontend Desa | Process View and Route Forbidden

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,11 +8,17 @@ import { Routes, Route } from "react-router-dom";
 // import view login
 import Login from '../views/Auth/Login';
 
+// import view forbidden
+import Forbidden from '../views/Auth/Forbidden';
+
 export default function RoutesIndex() {
     return (
         <Routes>
             {/* route "/login" */}
             <Route path="/login" element={<Login />} />
+
+            {/* route "/forbidden" */}
+            <Route path="/forbidden" element={<Forbidden />} />
         </Routes>
     );
 }
