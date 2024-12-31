982337a57fd72ce7a4604a11a31aa3ea8b0dec23
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-01 01:13:33 +0700

    Route Dashboard Configure | add view Dashboard

---
+++ src/views/Admin/Dashboard/Index.jsx
@@ -0,0 +1,10 @@
+// import Layout
+import LayoutAdmin from "../../../layouts/Admin"
+
+export default function Dashboard() {
+    return (
+        <LayoutAdmin>
+            <h1>Halaman Dashboard</h1>
+        </LayoutAdmin>
+    );
+}
