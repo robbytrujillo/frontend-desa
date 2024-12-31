468977fdd74d6a15cdf8aa9c578343a79d0f91f6
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-01 01:18:12 +0700

    Route Dashboard Configure | add view Dashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,9 @@
 // import react router dom
 import { Routes, Route } from "react-router-dom";
 
+// import private routes
+import PrivateRoutes from "./PrivateRoutes";
+
 //======================================================
 // view admin
 //======================================================
@@ -11,6 +14,9 @@ import Login from '../views/Auth/Login';
 // import view forbidden
 import Forbidden from '../views/Auth/Forbidden';
 
+// import view dashboard
+import Dashboard from "../views/Admin/Dashboard/Index";
+
 export default function RoutesIndex() {
     return (
         <Routes>
@@ -19,6 +25,16 @@ export default function RoutesIndex() {
 
             {/* route "/forbidden" */}
             <Route path="/forbidden" element={<Forbidden />} />
+
+            {/* private route "/admin/dashboard" */}
+            <Route 
+                path="/admin/dashboard" 
+                element={
+                    <PrivateRoutes> 
+                    <Dashboard />
+                    </PrivateRoutes>
+                }
+            />
         </Routes>
     );
 }
