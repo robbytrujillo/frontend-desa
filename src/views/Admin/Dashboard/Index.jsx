d0c808be8339b411f9c4feebfcc5cbb0b7f2bbcb
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-01 10:16:15 +0700

    Route Dashboard Configure | add view Dashboard

--- src/views/Admin/Dashboard/Index.jsx
+++ src/views/Admin/Dashboard/Index.jsx
@@ -50,7 +50,66 @@ export default function Dashboard() {
             <main>
                 <div class="container-fluid px-4 mt-5">
                     <div class="row">
-                        
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4 border-0 shadow-sm">
+                                <div class="card-body">
+                                    <strong>{countCategories}</strong> CATEGORIES
+                                </div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <Link class="small text-white stretched-link" to="/admin/categories">
+                                        View Details
+                                    </Link>
+                                    <div class="small text-white">
+                                        <i class="fas fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-warning text-white mb-4 border-0 shadow-sm">
+                                <div class="card-body">
+                                    <strong>{countPosts}</strong> POSTS
+                                </div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <Link class="small text-white stretched-link" to="/admin/posts">
+                                    View Details
+                                    </Link>
+                                    <div class="small text-white">
+                                        <i class="fas fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-success text-white mb-4 border-0 shadow-sm">
+                                <div class="card-body">
+                                    <strong>{countProducts}</strong> PRODUCTS
+                                </div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <Link class="small text-white stretched-link" to="/admin/products">
+                                        View Details
+                                    </Link>
+                                    <div class="small text-white">
+                                        <i class="fas fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-danger text-white mb-4 border-0 shadow-sm">
+                                <div class="card-body">
+                                    <strong>{countAparaturs}</strong> APARATURS
+                                </div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <Link class="small text-white stretched-link" to="/admin/aparaturs">
+                                        View Details
+                                    </Link>
+                                    <div class="small text-white">
+                                        <i class="fas fa-angle-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
