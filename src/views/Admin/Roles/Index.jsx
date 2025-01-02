281921dde03b29b0658f2cb8a023d9e864306677
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-03 01:38:04 +0700

    Edit View Roles Index

--- src/views/Admin/Roles/Index.jsx
+++ src/views/Admin/Roles/Index.jsx
@@ -20,9 +20,52 @@ import hasAnyPermission from "../../../utils/Permissions";
 import Pagination from "../../../components/general/Pagination";
 
 export default function RolesIndex() {
-    return (
-        <LayoutAdmin>
-            <h1>Halaman Roles Index</h1>
-        </LayoutAdmin>
-    );
+    // title page
+    document.title = "roles - Desa Digital";
+
+    // define state "roles"
+    const [roles, setRoles] = useState([]);
+
+    // define state "pagination"
+    const [pagination, setPagination] = useState({ 
+        currentPage: 0,
+        per_page: 0,
+        total: 0,  
+    });
+
+    // define state "keywords"
+    const [keywords, setKeywords] = useState("");
+
+    // token from cookies
+    const token = Cookies.get("token");
+
+    // function fetchData
+    const fetchData = async (pageNumber = 1, keywords = "") => {
+        // define variable "page"
+        const page = pageNumber ? pageNumber : pagination.currentPage;
+
+        await Api.get(`/api/admin/roles?search=${keywords}&page=$page}`, {
+            // header
+            headers: {
+                // header Bearer + Token
+                "Authorization": `Bearer ${token}`,
+            },
+        }).then((response) => {
+            // set data response to state "setRoles"
+            setRoles(response.data.data.data);
+
+            // set data pagination to state "pagination"
+            setPagination(() => ({
+                currentPage: response.data.data.current_page,
+                perPage: response.data.data.per_page,
+                total: response.data.data.total,
+        }));
+    });
+};
+
+    // return (
+    //     <LayoutAdmin>
+    //         <h1>Halaman Roles Index</h1>
+    //     </LayoutAdmin>
+    // );
 }
