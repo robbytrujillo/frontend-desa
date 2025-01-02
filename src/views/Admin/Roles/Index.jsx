c0ccdf761ca5a72586690e9cb561aea2b8ff5162
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-03 01:20:06 +0700

    Edit View Roles Index

--- src/views/Admin/Roles/Index.jsx
+++ src/views/Admin/Roles/Index.jsx
@@ -1,8 +1,23 @@
 // import useState and useEffect
 import { useState, useEffect } from "react";
 
+// import Link from react router dom
+import { Link } from "react-router-dom";
+
+// import api
+import Api from "../../../services/Api";
+
+// import js cookie
+import Cookies from "js-cookie";
+
 // import layout
 import LayoutAdmin from "../../../layouts/Admin";
+
+// import permissions
+import hasAnyPermission from "../../../utils/Permissions";
+
+// import pagination component
+import Pagination from "../../../components/general/Pagination";
 
 export default function RolesIndex() {
     return (
