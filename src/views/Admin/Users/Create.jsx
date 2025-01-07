aeb90fa1da1a181c2656d56f0a6280a264abd6f6
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-07 13:56:45 +0700

    add View and Route Users Create

--- src/views/Admin/Users/Create.jsx
+++ src/views/Admin/Users/Create.jsx
@@ -110,10 +110,145 @@ export default function UserCreate() {
 
     return (
         <LayoutAdmin>
-            <h1>Halaman Users Create</h1>
+            {/* <h1>Halaman Users Create</h1> */}
             <main>
-                <div className="contaiener-fluid">
-                    
+                <div className="contaiener-fluid mb-5 mt-5">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link 
+                                to="/users"
+                                className="btn btn-md btn-primary border-0 shadow-sm mb-3"
+                                type="button"
+                            >
+                                <i className="fa fa-long-arrow-alt-left me-2"></i> Back
+                            </Link>
+                            <div className="card border-0 rounded shadow-sm border-top-success">
+                                <div className="card-body">
+                                    <h6>
+                                        <i className="fa fa-user"></i> Create User
+                                    </h6>
+                                    <hr />
+                                    <form onSubmit={storeUser}>
+                                        <div className="row">
+                                            <div className="col-md-6">
+                                                <div className="mb-3">
+                                                    <label className="form-label fw-bold">
+                                                        Full Name
+                                                    </label>
+                                                    <input 
+                                                        type="text"
+                                                        className="form-control"
+                                                        value={name}
+                                                        onChange={(e) => setName(e.target.value)}
+                                                        placeholder="Enter Full Name"
+                                                    />
+                                                </div>
+                                                {errors.name && (
+                                                    <div className="alert alert-danger">
+                                                        {errors.name[0]}
+                                                    </div>
+                                                )}
+                                            </div>
+                                            <div className="col-md-6">
+                                                <div className="mb-3">
+                                                    <label className="form-label fw-bold">
+                                                        Email Address
+                                                    </label>
+                                                    <input 
+                                                        type="text"
+                                                        className="form-control"
+                                                        value={email}
+                                                        onChange={(e) => setEmail(e.target.value)}
+                                                        placeholder="Enter Email Address"
+                                                    />
+                                                </div>
+                                                {errors.email && (
+                                                    <div className="alert alert-danger">
+                                                        {errors.email[0]}
+                                                    </div>
+                                                )}
+                                            </div>            
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col-md-6">
+                                                <div className="mb-3">
+                                                    <label className="form-label fw-bold">Password</label>
+                                                    <input
+                                                        type="password"
+                                                        className="form-control"
+                                                        value={password}
+                                                        onChange={(e) => setPassword(e.target.value)}
+                                                        placeholder="Enter Password"
+                                                        />
+                                                </div>
+                                                {errors.password && ( 
+                                                    <div className="alert alert-danger">
+                                                        {errors.password[0]}
+                                                    </div>
+                                                )}
+                                            </div>
+                                            <div className="col-md-6">
+                                                <div className="mb-3">
+                                                    <label className="form-label fw-bold">
+                                                        Password Confirmation
+                                                    </label>
+                                                    <input 
+                                                        type="password"
+                                                        className="form-control"
+                                                        value={passwordConfirmation}
+                                                        onChange={(e) => setPasswordConfirmation(e.target.value)}
+                                                        placeholder="Enter Password Confirmation"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                        <div className="mb-3">
+                                            <label className="fw-bold">Roles</label>
+                                            <br />
+                                            {roles.map((role) => (
+                                                <div
+                                                    className="form-check form-check-inline"
+                                                    key={Math.random()}
+                                                 >
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        value={role.name}
+                                                        onChange={handleCheckboxChange}
+                                                        id={`check-${role.id}`}
+                                                    />
+                                                    <label
+                                                        className="form-check-label fw-normal"
+                                                        htmlFor={`check-${role.id}`}
+                                                    >
+                                                        {role.name}
+                                                    </label>
+                                                 </div>
+                                                ))}
+
+                                                {errors.roles && (
+                                                    <div className="alert alert-danger mt-2">
+                                                        {errors.roles[0]}
+                                                    </div>    
+                                                )}    
+                                        </div>
+                                        <div>
+                                            <button 
+                                                type="submit"
+                                                className="btn btn-md btn-primary me-2">
+                                                    <i className="fa fa-save"></i> Save
+                                            </button>
+                                            <button type="reset" className="btn btn-md btn-warning">
+                                                <i className="fa fa-redo"></i> Reset
+                                            </button>
+                                        </div>   
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </LayoutAdmin>
