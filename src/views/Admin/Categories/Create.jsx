d915b468f8e10af69c885831695f0cdbb07fa37b
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-10 07:47:32 +0700

    Add Proses Create Data Category | Update View Category Create

--- src/views/Admin/Categories/Create.jsx
+++ src/views/Admin/Categories/Create.jsx
@@ -70,6 +70,59 @@ export default function CategoriesCreate() {
     return (
         <LayoutAdmin>
             <h1>Halaman Categories Create</h1>
+            <main>
+                <div className="container-fluid mb-5 mt-5">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link 
+                                to="/admin/categories"
+                                className="btn btn-md btn-primary border-0 shadow-sm mb-3"
+                                type="button"
+                            >
+                                <i className="fa fa-long-arrow-alt-left me-2"></i> Back
+                            </Link>
+                            <div className="card border-0 rounded shadow-sm border-top-success">
+                                <div className="card-body">
+                                    <h6>
+                                        <i className="fa fa-folder"></i> Create Category
+                                    </h6>
+                                    <hr />
+                                    <form onSubmit={storeCategory}>
+                                        <div className="mb-3">
+                                            <label className="form-label fw-bold">
+                                                Category Name
+                                            </label>
+                                            <input 
+                                                type="text"
+                                                className="form-control"
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                                placeholder="Enter Category Name"
+                                            />
+                                        </div>
+                                        {errors.name && (
+                                            <div className="alert alert-danger">{errors.name[0]}</div>
+                                        )}
+                                        <div>
+                                            <button 
+                                            type="submit"
+                                            className="btn btn-md btn-primary me-2"
+                                            >
+                                                <i className="fa fa-save"></i> Save
+                                            </button>
+                                            <button type="reset"
+                                            className="btn btn-md btn-warning"
+                                            >
+                                                <i className="fa fa-redo"></i> Reset
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+            </main>
         </LayoutAdmin>
     );
 }
