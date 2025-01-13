ed7d632b517907bd02f7372c5701b6589a9b88a8
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-13 07:34:03 +0700

    Add Process Create Data Post | Update view post create

--- src/views/Admin/Posts/Create.jsx
+++ src/views/Admin/Posts/Create.jsx
@@ -1,5 +1,26 @@
+// import react
+import { useState, useEffect } from "react";
+
+// import react router dom
+import { Link, useNavigate } from "react-router-dom";
+
 // import layout
 import LayoutAdmin from "../../../layouts/Admin";
+
+// import api
+import Api from "../../../services/Api";
+
+// import js cookie
+import Cookies from "js-cookie";
+
+// import toast
+import toast from "react-hot-toast"
+
+// import react Quill
+import ReactQuill from 'react-quill';
+
+// quill CSS
+import 'react-quill/dist/quill.snow.css';
 
 export default function PostsCreate() {
     return (
