ac417762877a77fe5aa7e2149a54ac673323a9ac
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-18 08:48:27 +0700

    Add Proses Create Data Page | Update View Pages Create

--- src/views/Admin/Pages/Create.jsx
+++ src/views/Admin/Pages/Create.jsx
@@ -77,8 +77,72 @@ export default function PagesCreate() {
         <LayoutAdmin>
             <h1>Halaman Pages Create</h1>
             <main>
-                
+                <div className="container-fluid mb-5 mt-5">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link 
+                            to="/admin/pages"
+                            className="btn btn-md btn-primary border-0 shadow-sm mb-3"
+                            type="button"
+                            >
+                                <i className="fa fa-long-arrow-alt-left me-2"></i> Kembali
+                            </Link>
+                            <div className="card border-0 rounded shadow-sm border-top-success">
+                                <div className="card-body">
+                                    <h6>
+                                        <i className="fa fa-pencil-alt"></i> Create Page
+                                    </h6>
+                                    <hr />
+                                    <form onSubmit={storePage}>
+                                        <div className="mb-3">
+                                            <label className="form-label fw-bold">Title</label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                value={title}
+                                                onChange={(e) => setTitle(e.target.value)}
+                                                placeholder="Enter Title Page"
+                                            />
+                                        </div>
+                                        {errors.title && (
+                                            <div className="alert alert-danger">
+                                                {errors.title[0]}
+                                            </div>
+                                        )}
+
+                                        <div className="mb-3">
+                                            <label className="form-label fw-bold">Content</label>
+                                            <ReactQuill
+                                                theme="snow"
+                                                rows="5"
+                                                value={content}
+                                                onChange={(content) => setContent(content)}
+                                            />
+                                        </div>
+                                        {errors.content && (
+                                            <div className="alert alert-danger">
+                                                {errors.content[0]}
+                                            </div>
+                                        )}
+
+                                        <div>
+                                            <button
+                                                type="submit"
+                                                className="btn btn-md btn-primary me-2"
+                                            >
+                                                <i className="fa fa-save"></i> Save
+                                            </button>
+                                            <button type="reset" className="btn btn-md btn-warning">
+                                                <i className="fa fa-redo"></i> Reset
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
         </LayoutAdmin>
-    )
+    );
 }
