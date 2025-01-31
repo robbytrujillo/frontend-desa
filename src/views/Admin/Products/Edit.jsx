c58a9d0eab9e2a5ee421e31d11d0c569440e5924
robbytrujillo <robbyikusuma88@gmail.com>
2025-02-01 06:06:09 +0700

    Route Product Edit Configuration | Add view products edit

---
+++ src/views/Admin/Products/Edit.jsx
@@ -0,0 +1,10 @@
+// import Layout
+import LayoutAdmin from "../../../layouts/Admin";
+
+export default function ProdutsEdit() {
+    return (
+        <LayoutAdmin>
+            <h1>Halaman Products Edit</h1>
+        </LayoutAdmin>
+    )
+}
