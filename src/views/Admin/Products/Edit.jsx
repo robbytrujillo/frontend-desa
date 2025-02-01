9afe52f305e01cdc484ebb204d651dc63cbc2f0a
robbytrujillo <robbyikusuma88@gmail.com>
2025-02-02 04:39:20 +0700

    Add Process Edit Data Product | Edit View Product Edit

--- src/views/Admin/Products/Edit.jsx
+++ src/views/Admin/Products/Edit.jsx
@@ -42,6 +42,28 @@ export default function ProdutsEdit() {
     const [phone, setPhone] = useState("");
     const [errors, setErros] = useState([]);
 
+    // token from cookies
+    const token = Cookies.get("token");
+
+    // function "fetchDataProduct"
+    const fetchDataProduct = async () => {
+        await Api.get(`/api/admin/products/${id}`, {
+            // header
+            headers: {
+                // header Bearer + Token
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            // set response data to state
+            setTitle(response.data.data.title);
+            setContent(response.data.data.content);
+            setOwner(response.data.data.owner);
+            setPrice(response.data.data.price);
+            setAddress(response.data.data.address);
+            setPhone(response.data.data.phone);
+        });
+    };
+    
     return (
         <LayoutAdmin>
             <h1>Halaman Products Edit</h1>
