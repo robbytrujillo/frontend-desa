8e10a4299df1fdf578a4a31b5c3d37430528649b
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-29 05:22:06 +0700

    Desa Aplication | Create View Photos Index

---
+++ src/views/Admin/Photos/Index.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutAdmin from "../../../layouts/Admin";
+
+export default function PhotosIndex() {
+    return (
+        <LayoutAdmin>
+            <h1>Halaman Photos Index</h1>
+        </LayoutAdmin>
+    );
+}
