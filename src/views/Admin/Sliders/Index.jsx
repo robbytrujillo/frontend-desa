18b30249c66b12fb3e1b73988dca8233b17dd5d6
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-31 04:41:24 +0700

    Desa Aplication | Update View Sliders Index

--- src/views/Admin/Sliders/Index.jsx
+++ src/views/Admin/Sliders/Index.jsx
@@ -1,10 +1,142 @@
+// import useState and useEffect
+import { useState, useEffect } from 'react';
+
+// import api
+import Api from "../../../services/Api";
+
+// import js cookie
+import Cookies from 'js-cookie';
+
 // import layout
 import LayoutAdmin from "../../../layouts/Admin";
 
+// import permissions
+import hasAnyPermission from '../../../utils/Permissions';
+
+// import pagination component
+import Pagination from '../../../components/general/pagination';
+
 export default function SlidersIndex() {
+    // title page
+    document.title = "Sliders - Desa Digital";
+
+    // define state "sliders"
+    const [sliders, setSliders] = useState([]);
+
+    // define state "pagination"
+    const [pagination, setPagination] = useState({ 
+        currentPage: 0,
+        perPage: 0,
+        total: 0,
+    });
+
+    // token from cookies
+    const token = Cookies.get('token');
+
+    // function fetchData
+    const fetchData = async (pageNumber = 1) => {
+        // define variable "page"
+        const page = pageNumber ? pageNumber : pagination.currentPage;
+
+        await Api.get(`/api/admin/sliders?page=${page}`, {
+            // header
+            headers: {
+                 // header Bearer + Token
+                Authorization: `Bearer ${token}`,
+            },           
+        }).then((response) => {
+            // set data response to state "setSliders"
+            setSliders(response.data.data.data);
+
+            // set data pagination to state "pagination"
+            sertPagination(() => ({
+                currentPage: response.data.data.current_page,
+                perPage: response.data.data.per_page,
+                total: response.data.data.total,
+            }));
+        });
+    };
+
+    // useEffect
+    useEffect(() => {
+        // call function "fetchData"
+        fetchData();
+    }, []);
+
     return (
         <LayoutAdmin>
-            <h1>Halaman Sliders Index</h1>
+            {/* <h1>Halaman Sliders Index</h1> */}
+            <main>
+                <div className="container-fluid mb-5 mt-5">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="card border-0 rounded shadow-sm border-top-success">
+                                <div className="card-body">
+                                    <div className="table-responsive">
+                                        <table className="table table-bordered table-centered mb-0 rounded">
+                                            <thead className="thead-dark">
+                                                <tr className="border-0">
+                                                    <th className="border-0" style={{ width: "5%" }}>No.</th>
+                                                    <th className="border-0" >Image</th>
+                                                    <th className="border-0" style={{ width: "15%" }}>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {
+                                                    // cek apakah data ada
+                                                    sliders.length > 0 ? (
+                                                        // looping data "sliders" dengan "map"
+                                                    sliders.map((slider, index) => (
+                                                        <tr key={index}>
+                                                            <td className="fw-bold text-center">
+                                                                {++index + 
+                                                                    (pagination.currentPage - 1) *
+                                                                    pagination.perPage}
+                                                            </td>
+                                                            <td className="text-center">
+                                                                <img
+                                                                    src={sliders.image}
+                                                                    width={"300px"}
+                                                                    className="rounded"
+                                                                />
+                                                            </td>
+                                                            <td className="text-center">
+                                                                {hasAnyPermission(["sliders.delete"]) && (
+                                                                    <button className="btn btn-danger btn-sm">
+                                                                        <i className="fa fa-trash"></i>
+                                                                    </button>
+                                                                )}
+                                                            </td>
+                                                        </tr>
+                                                    ))
+                                                ) : (
+                                                    // tampilkan pesan data belum tersedia
+                                                    <tr>
+                                                        <td colSpan={5}>
+                                                            <div 
+                                                            className="alert alert-danger border-0 rounded shadow-sm w-100 text-center" role='alert'>
+                                                                Data Belum Tersedia!.
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <Pagination
+                                        currentPage={pagination.currentPage}
+                                        perPage={pagination.perPage}
+                                        total={pagination.total}
+                                        onChange={(pageNumber) => fetchData(pageNumber, keywords)}
+                                        position="end"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </LayoutAdmin>
     );
 }
