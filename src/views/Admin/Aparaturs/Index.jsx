5e924ba023a6b63b643121708ed32a682a0ca9ff
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-01 20:19:39 +0700

    Desa Aplication | Update View Aparaturs Index

--- src/views/Admin/Aparaturs/Index.jsx
+++ src/views/Admin/Aparaturs/Index.jsx
@@ -20,6 +20,64 @@ import hasAnyPermission from "../../../utils/Permissions";
 import Pagination from "../../../components/general/Pagination";
 
 export default function AparatursIndex() {
+    // title page
+    document.title = "Aparaturs - Desa Digital";
+
+    // define state "aparaturs"
+    const [aparaturs, setAparaturs] = useState([]);
+
+    // define state "pagination"
+    const [pagination, setPagination] = useState({
+        currentPage: 0,
+        perPage: 0,
+        total: 0,
+    });
+
+    // define state "keywords"
+    const [keywords, setKeywords] = useState("");
+
+    // token from cookies
+    const token = Cookies.get('token');
+
+    // function fetchData
+    const fetchData = async (pageNumber = 1, keywords="") => {
+        // define variable "page"
+        const page = pageNumber ? pageNumber : pagination.currentPage;
+
+        await Api.get(`/api/admin/aparaturs?search=${keywords}&page=${page}`, {
+            // header
+            headers: {
+                // header Bearer + Token
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            // set data response to state "setAparaturs"
+            setAparaturs(response.data.data.data);
+
+            // set data pagination to state "pagination"
+            setPagination(() => ({
+                currentPage: response.data.data.current_page,
+                perPage: response.data.data.per_page,
+                total: response.data.data.total,
+                }));
+            });
+        };
+        
+        // useEffect
+        useEffect(() => {
+            // call function "fetchData"
+            fetchData();
+        }, []);
+
+        // function "searchData"
+        const searchData = async (e) => {
+            // set value to state "keywords"
+            setKeywords(e.target.value);
+
+            // call function "fetchData"
+            fetchData(1, e.target.value);
+        };
+
     return (
         <LayoutAdmin>
             <h1>Halaman Aparaturs Index</h1>
