f3103f94e058be5c01236f77d7b33faebdf35cab
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-02 06:03:12 +0700

    Desa Aplication | Create View Aparatur Create

---
+++ src/views/Admin/Aparaturs/Create.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutAdmin from "../../../layouts/Admin";
+
+export default function AparatursCreate() {
+    return (
+        <LayoutAdmin>
+            <h1>Halaman Aparaturs Create</h1>
+        </LayoutAdmin>
+    );
+}
