bc47abe5c100aed77f2454bb62569a8ed627fefd
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-05 10:15:00 +0700

    Desa Aplication | Create View Aparatur Edit

---
+++ src/views/Admin/Aparaturs/Edit.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutAdmin from "../../../layouts/Admin";
+
+export default function AparatursEdit() {
+    return (
+        <LayoutAdmin>
+            <h1>Halaman Aparaturs Edit</h1>
+        </LayoutAdmin>
+    );
+}
