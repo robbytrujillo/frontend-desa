60560ea68833d4fd3ec094fcbd655bebd14b7e95
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-05 23:12:15 +0700

    Desa Aplication | Update View  Aparatur Edit

--- src/views/Admin/Aparaturs/Edit.jsx
+++ src/views/Admin/Aparaturs/Edit.jsx
@@ -69,9 +69,57 @@ export default function AparatursEdit() {
         formData.append("role", role);
         formData.append("_method", "PUT");
 
+        // sending data
+        await Api.put(`/api/admin/aparaturs/${id}`, formData, {
+            // header
+            headers: {
+                // header Bearer + Token
+                "Authorization": `Bearer ${token}`,
+                "content-type": "multipart/form-data",
+            },
+        })
+        .then((response) => {
+            // show toast
+            toast.success(response.data.message, {
+                position: "top-right",
+                duration: 4000,
+            });
+
+            // redirect
+            navigate("/admin/aparaturs");
+        })
+        .catch((error) => {
+            // set error message to state "errors"
+            setErrors(error.response.data);
+        });
+    };
+
     return (
         <LayoutAdmin>
-            <h1>Halaman Aparaturs Edit</h1>
+            {/* <h1>Halaman Aparaturs Edit</h1> */}
+            <main>
+                <div className="container-fluid mb-5 mt-5">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Link
+                                to="/admin/aparaturs"
+                                className="btn byn-md btn-primary border-0 shadow-sm mb-3"
+                                type="button"
+                            >
+                                <i className="fa fa-long-arrow-alt-left me-2"></i> Back
+                            </Link>
+                            <div className="card border-0 rounded shadow-sm border-top-success">
+                                <div className="card-body">
+                                    <h6>
+                                        <i className="fa fa-pencil"></i> Edit Aparatur
+                                    </h6>
+                                    <hr />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </LayoutAdmin>
     );
 }
