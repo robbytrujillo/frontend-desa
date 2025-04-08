ce52338c2ea01e8cadbae9e94c933e76a3a486e8
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-09 05:47:39 +0700

    Desa Aplication | Create Component Navbar Web

--- src/components/web/Navbar.jsx
+++ src/components/web/Navbar.jsx
@@ -16,7 +16,28 @@ export default function Navbar() {
         <>
             <nav className="navbar navbar-expand-md navbar-light navbar-top d-none d-md-block d-lg-block">
                 <div className="container">
-                    
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                            <li className="nav-item me-4">
+                                <i className="fa fa-envelope"></i> info@desa-digital.com
+                            </li>
+                            <li className="nav-item me-4">
+                                <i className="fa fa-phone"></i> +62 813 6794 3830 
+                            </li>
+                        </ul>
+                        <div>
+                            IKUTI KAMI :
+                            <a href="#" className="ms-2 me-2">
+                                <i className="fa fa-facebook-square text-white fa-lg"></i>
+                            </a>
+                            <a href="http://" className="ms-2 me-2">
+                                <i className="fa fa-instagram text-white fa-lg"></i>
+                            </a>
+                            <a href="#" className="ms-2 me-2">
+                                <i className="fa fa-youtube text-white fa-lg"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </>
