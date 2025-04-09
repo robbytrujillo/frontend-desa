e2f61b059930f1b5bd1760857e593ed4dbd670ad
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-09 23:03:07 +0700

    Desa Aplication | Create Component Navbar Web

--- src/components/web/Navbar.jsx
+++ src/components/web/Navbar.jsx
@@ -99,7 +99,83 @@ export default function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarCollapse">
                         <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                            <li className="nav-item ms-2">
+                                <Link
+                                    className={
+                                        activeRoute[1] === ""
+                                            ? "nav-link active text-uppercase"
+                                            : "nav-link text-uppercase"
+                                    }
+                                    to="/"
+                                >
+                                    <i className="fa fa-home"></i> HOME
+                                </Link>
+                            </li>
+
+                            <li className = "nav-item ms-2">
+                                <Link
+                                    className={
+                                        activeRoute[1] === "pages"
+                                        ? "nav-link active text-uppercase"
+                                        : "nav-link text-uppercase"
+                                        }
+                                        to="/pages"
+                                    >
+                                        <i className="fa fa-info-circle"></i> TENTANG DESA
+                                </Link>
+                            </li>
+
+                            <li className="nav-item ms-2">
+                                <Link
+                                className={
+                                    activeRoute[1] === "aparaturs" 
+                                    ? "nav-link active text-uppercase"
+                                    : "nav-link text-uppercase"
+                                }
+                                to="/aparaturs"
+                                >
+                                    <i className="fa fa-user-circle"></i> APARATURS
+                                </Link>
+                            </li>
                             
+                            <li className="nav-item ms-2">
+                                <Link
+                                className={
+                                    activeRoute[1] === "posts" 
+                                    ? "nav-link active text-uppercase"
+                                    : "nav-link text-uppercase"
+                                }
+                                to="/posts"
+                                >
+                                    <i className="fa fa-book"></i> BERITA
+                                </Link>
+                            </li>
+                            
+                            <li className="nav-item ms-2">
+                                <Link
+                                className={
+                                    activeRoute[1] === "products" 
+                                    ? "nav-link active text-uppercase"
+                                    : "nav-link text-uppercase"
+                                }
+                                to="/products"
+                                >
+                                    <i className="fa fa-shooping-bag"></i> PRODUCTS
+                                </Link>
+                            </li>
+                            
+                            <li className="nav-item ms-2">
+                                <Link
+                                className={
+                                    activeRoute[1] === "photos" 
+                                    ? "nav-link active text-uppercase"
+                                    : "nav-link text-uppercase"
+                                }
+                                to="/photos"
+                                >
+                                    <i className="fa fa-images"></i> GALERI
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
