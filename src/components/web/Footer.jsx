340b021eaf02026904305a9793bf453dd2494afb
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-09 23:11:33 +0700

    Desa Aplication | Create Component Navbar Web

---
+++ src/components/web/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function footer() {
+    return (
+        <footer>
+            <div className="container-fluif footer-top">
+                <div className="row p-4">
+                    <div className="col-md-4 mb-4 mt-3">
+                        <h5>
+                            TENTANG
+                            <strong style={{ color: "#ffd22e" }}> DESA DIGITAL </strong>
+                        </h5>
+                        <hr />
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
