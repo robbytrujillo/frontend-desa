b3b2859fc02653eb08c3fc73bc1a4ab693514841
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-11 22:36:06 +0700

    Desa Aplication | Create Component Sliders

---
+++ src/components/web/Slider.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+// import api service
+import Api from "../../services/Api";
+
+// import component carousel
+import { Carousel } from 'react-bootstrap';
