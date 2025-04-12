b974b1830a134c2d93bfdbab0bc2c54779f5c848
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-12 13:12:39 +0700

    Desa Aplication | Create Helpers MoneyFormat

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -7,7 +7,8 @@ import Cookies from "js-cookie";
 const Api = axios.create({
 
     // set endpoint API
-    baseURL: 'http://desa-api.robbyilham.com',
+    // baseURL: 'http://desa-api.robbyilham.com',
+    baseURL: 'http://127.0.0.1:8000/',
 
     // set header axios
     headers: {
