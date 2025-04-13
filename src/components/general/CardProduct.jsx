796bc3cb19b2b3f0f4f7d5dd669e121e1407d64e
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-13 21:33:31 +0700

    Desa Aplication | Create Component CardProduct

--- src/components/general/CardProduct.jsx
+++ src/components/general/CardProduct.jsx
@@ -6,3 +6,15 @@ import MoneyFormat from "../../utils/MoneyFormat";
 
 // import link
 import { Link } from "react-router-dom";
+
+export default function CardProduct(props) {
+    return (
+        <div className="col-md-4 mb-3" key={props.key}>
+            <Link to={`/products/${props.slug}`} className="text-decoration-none">
+                <div class="card mb-3 w-100 rounded-3 border-0 shadow-sm">
+                    <img src={props.image} class="card-img-top" alt="..." />
+                </div>
+            </Link>
+        </div>
+    )
+}
