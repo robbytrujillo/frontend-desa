deb59487857d6a8acd59e1aeb1bc6ccc0f8ea73d
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-17 05:50:04 +0700

    Desa Aplication | Create View Aparaturs Index Web

---
+++ src/views/Web/Aparaturs/Index.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutWeb from "../../../layouts/Web";
+
+export default function Aparaturs() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Aparaturs</h1>
+        </LayoutWeb>
+    );
+}
