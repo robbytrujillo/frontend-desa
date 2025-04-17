74336bebad3d9230e8fd0c6c92d1ae4d50a73116
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-18 04:45:37 +0700

    Desa Aplication | Display  Aparaturs Data

--- src/views/Web/Aparaturs/Index.jsx
+++ src/views/Web/Aparaturs/Index.jsx
@@ -1,5 +1,20 @@
+// import react
+import React, { useState, useEffect } from 'react';
+
 // import layout
 import LayoutWeb from "../../../layouts/Web";
+
+// import service api
+import Api from '../../../services/api';
+
+// import component alert
+import AlertDataEmpty from '../../../components/general/AlertDataEmpty';
+
+// import component Loading
+import Loading from '../../../components/general/Loading';
+
+// import component card aparatur
+import CardAparatur from '../../../components/general/CardAparatur';
 
 export default function Aparaturs() {
     return (
