16ba653b302a172bbb96c084ded02f025e39b7e9
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-18 08:11:53 +0700

    Desa Aplication | Create View Pages Web Index

---
+++ src/views/Web/Pages/Index.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutWeb from "../../../layouts/Web";
+
+export default function WebPagesIndex() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Pages Index</h1>
+        </LayoutWeb>
+    );
+}
