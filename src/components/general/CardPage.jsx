b2cb5c7e4f8097184a0929379252e5d888a5f293
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-18 08:25:16 +0700

    Desa Aplication | Create Component CardPage

---
+++ src/components/general/CardPage.jsx
@@ -0,0 +1,22 @@
+// import React
+import React from "react";
+
+// import link
+import { Link } from "react-router-dom";
+
+export default function CardPage(props) {
+    return (
+        <div className="col-md-4 mb-4">
+            <Link to={`/pages/${props.slug}`} className="text-decoration-none">
+                <div
+                    className="card text-uppercase border-0 shadow-sm rounded-3 text-center"
+                    key={props.key}
+                >
+                    <div className="card-body mt-2">
+                        <h5>{props.title}</h5>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
