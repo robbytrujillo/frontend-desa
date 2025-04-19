daad2e9dcb84f4edcdc3de7b37c027666e81f6b4
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-20 06:40:10 +0700

    Desa Aplication | Display Data Pages Web

--- src/views/Web/Pages/Index.jsx
+++ src/views/Web/Pages/Index.jsx
@@ -24,10 +24,42 @@ export default function WebPagesIndex() {
     // init state
     const [pages, setPages] = useState([]);
     const [loadingPages, setLoadingPages] = useState(true);
-    
+
+    // fetch data pages
+    const fetchDataPages = async () => {
+        // setLoadingPages "true"
+        setLoadingPages(true);
+
+        // fetch data
+        await Api.get("/api/public/pages").then((response) => {
+            // assign response to state
+            setPages(response.data.data);
+
+            // setLoadingPages "false"
+            setLoadingPages(false);
+        });
+    }
+
+    // hook useEffect
+    useEffect(() => {
+        // call function "fetchDataPages"
+        fetchDataPages();
+    }, []);
+
     return (
         <LayoutWeb>
-            <h1>Halaman Pages Index</h1>
+            {/* <h1>Halaman Pages Index</h1> */}
+            <div className="container mt-4 mb-3">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h5 className="text-uppercase">
+                            <i className="fa fa-info-circle"></i> TENTANG DESA
+                        </h5>
+                        <hr />
+                    </div>
+                </div>
+                
+            </div>
         </LayoutWeb>
     );
 }
