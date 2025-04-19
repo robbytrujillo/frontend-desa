16fed8842bc28affab7687d46e8686850e97536b
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-19 22:04:52 +0700

    Desa Aplication | Display Data Pages Web

--- src/views/Web/Pages/Index.jsx
+++ src/views/Web/Pages/Index.jsx
@@ -5,6 +5,18 @@ import React, { useState, useEffect } from "react";
 // import layout
 import LayoutWeb from "../../../layouts/Web";
 
+// import service api
+import Api from "../../../services/api";
+
+// import component aler
+import AlertDataEmpty from "../../../components/general/AlertDataEmpty";
+
+// import component loading
+import Loading from "../../../components/general/Loading";
+
+// import component card page
+import CardPage from "../../../components/general/CardPage";
+
 export default function WebPagesIndex() {
     return (
         <LayoutWeb>
