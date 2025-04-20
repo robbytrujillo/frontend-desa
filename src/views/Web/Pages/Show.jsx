c5f8b51d317a6d99eae9a4f8702499fe935014c5
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-20 16:57:55 +0700

    Desa Aplication | Update View Pages Web Show

--- src/views/Web/Pages/Show.jsx
+++ src/views/Web/Pages/Show.jsx
@@ -1,5 +1,11 @@
+// import React
+import React, { useState, useEffect } from react;
+
 // import layouts
 import LayoutWeb from "../../../layouts/Web";
+
+// import service api
+import Api from "../../../services/api";
 
 export default function WebPagesShow() {
     return (
