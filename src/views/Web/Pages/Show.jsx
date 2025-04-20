d2bbfcee137b1e67aecd753f9ceca2bbce76f37b
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-20 16:47:30 +0700

    Desa Aplication | Create Page Web Show View

---
+++ src/views/Web/Pages/Show.jsx
@@ -0,0 +1,10 @@
+// import layouts
+import LayoutWeb from "../../../layouts/Web";
+
+export default function WebPagesShow() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Page Show</h1>
+        </LayoutWeb>
+    );
+}
