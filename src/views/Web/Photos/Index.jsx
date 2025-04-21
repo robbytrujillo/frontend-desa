7bf58626f5640aec4d06082a5f4266bc3dfac9df
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-22 05:40:04 +0700

    Desa Aplication | View Data Photos

--- src/views/Web/Photos/Index.jsx
+++ src/views/Web/Photos/Index.jsx
@@ -1,5 +1,19 @@
+// import React
+import React, { useState, useEffect } from 'react';
+
 // import layout
 import LayoutWeb from "../../../layouts/Web";
+
+// import service api
+import Api from "../../../services/Api";
+
+// import component alert
+import AlertDataEmpty from "../../../components/general/AlertDataEmpty";
+
+// import component loading
+import Loading from "../../../components/general/CardPhoto";
+
+
 
 export default function WebPhotosIndex() {
     return (
