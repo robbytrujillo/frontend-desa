7557a94b7e84f9fa442b446ebb47655a8de7c6f6
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-23 19:07:03 +0700

    Desa Aplication | Create View Posts Web Index

---
+++ src/views/Web/Posts/Index.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutWeb from "../../../layouts/Web";
+
+export default function WebPostsIndex() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Posts Index</h1>
+        </LayoutWeb>
+    )
+}
