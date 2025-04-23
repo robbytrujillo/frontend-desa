e3aefab68180f7d88b797d261b2872b308cf0fdf
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-24 05:58:22 +0700

    Desa Aplication | Create Component CardPost

---
+++ src/components/general/CardPost.jsx
@@ -0,0 +1,16 @@
+// import React
+import React from "react";
+
+// import DateID
+import DateID from "../../utils/DateID";
+
+// import link
+import { Link } from "react-router-dom";
+
+export default function CardPost(props) {
+    return (
+        <div className="col-md-4 mb-3" key={props.key}>
+            
+        </div>
+    )
+}
