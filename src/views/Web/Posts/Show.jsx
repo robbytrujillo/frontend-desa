c9663d6b2ac5f33b3c74a6f564085c9eda3a04e3
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-27 05:48:11 +0700

    Desa Aplication | Create View Post Web Show

---
+++ src/views/Web/Posts/Show.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutWeb from "../../../layouts/Web";
+
+export default function WebPostsShow() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Post Show</h1>
+        </LayoutWeb>
+    )
+}
