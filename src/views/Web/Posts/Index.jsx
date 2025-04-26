fa238285b38f76e2b1cb1400733be2e517094409
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-26 16:30:10 +0700

    Desa Aplication | Display Data Posts

--- src/views/Web/Posts/Index.jsx
+++ src/views/Web/Posts/Index.jsx
@@ -19,8 +19,51 @@ import CardPost from "../../../components/general/CardPost";
 // import pagination component
 import Pagination from "../../../components/general/Pagination";
 
-
 export default function WebPostsIndex() {
+    // title page
+    document.title = "Berita - Desa Digital";
+
+    // init state
+    const [posts, setPosts] = useState([]);
+    const [loadingPost, setLoadingPost] = useState(true);
+
+    // define state "pagination"
+    const [pagination, setPagination] = useState({
+        currentPage: 0,
+        perPage: 0,
+        total: 0,
+    });
+
+    // fetch data posts
+    const fetchDataPosts = async (pageNumber = 1) => {
+        // setLoadingPost "true"
+        setLoadingPost(true);
+
+        // define variable "page"
+        const page = pageNumber ? pageNumber : pagination.currentPage;
+
+        await Api.get(`/api/public/posts?page=${page}`).then((response) => {
+            // assign response to state "posts"
+            setPosts(response.data.data.data);
+
+            // set data pagination to state "pagination"
+            setPagination(() => ({
+                currentPage: response.data.data.current_page,
+                perPage: response.data.data.data.per_page,
+                total: response.data.data.total,
+            }));
+
+            // setLoadingPost "false"
+            setLoadingPost(false);
+        });
+    };
+
+    // hook useEffect
+    useEffect(() => {
+        // call method "fetchDataPosts"
+        fetchDataPosts();
+    }, []);
+    
     return (
         <LayoutWeb>
             <h1>Halaman Posts Index</h1>
