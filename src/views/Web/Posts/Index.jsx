e4e181bc6771696fe0b71d01e4571c49c786af51
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-26 16:05:17 +0700

    Desa Aplication | Display Data Posts

--- src/views/Web/Posts/Index.jsx
+++ src/views/Web/Posts/Index.jsx
@@ -1,5 +1,24 @@
+// import React
+import React, { useState, useEffect } from "react";
+
 // import layout
 import LayoutWeb from "../../../layouts/Web";
+
+// import service api
+import Api from "../../../services/Api";
+
+// import component alert
+import AlertDataEmpty from "../../../components/general/AlertDataEmpty";
+
+// import component loading
+import Loading from "../../../components/general/Loading";
+
+// import component card post
+import CardPost from "../../../components/general/CardPost";
+
+// import pagination component
+import Pagination from "../../../components/general/Pagination";
+
 
 export default function WebPostsIndex() {
     return (
