0bd54bdeaed86d37f837b66f52a0a32095e33c93
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-29 14:41:17 +0700

    Desa Aplication | Update View Products Web Index

--- src/views/Web/Products/Index.jsx
+++ src/views/Web/Products/Index.jsx
@@ -10,6 +10,15 @@ import Api from "../../../services/Api";
 // import component alert
 import AlertDataEmpty from "../../../components/general/AlertDataEmpty";
 
+// import component loading
+import Loading from "../../../components/general/Loading";
+
+// import components card product
+import CardProduct from "../../../components/general/CardProduct";
+
+// import pagination component
+import Pagination from "../../../components/general/Pagination";
+
 export default function WebProductsIndex() {
     return (
         <LayoutWeb>
