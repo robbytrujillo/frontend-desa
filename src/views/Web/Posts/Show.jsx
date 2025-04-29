31f21c008e39f7a1c87206e43805e82b66881a64
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-29 10:14:27 +0700

    Desa Aplication | Update View Posts Web Show

--- src/views/Web/Posts/Show.jsx
+++ src/views/Web/Posts/Show.jsx
@@ -60,10 +60,65 @@ export default function WebPostsShow() {
             setLoadingPosts(false);
         });
     };
+
+    // hook useEffect
+    useEffect(() => {
+        // call method "fetchDetailDataPost"
+        fetchDetailDataPost();
+
+        // call method "fetchAllPosts"
+        fetchAllPosts();
+    }, [slug]);
     
     return (
         <LayoutWeb>
-            <h1>Halaman Post Show</h1>
+            {/* <h1>Halaman Post Show</h1> */}
+            <div className="container mt-4 mb-3">
+                <div className="row">
+                    <div className="col-md-8 mb-4">
+                        {loadingPost ? (
+                            <Loading />
+                        ) : (
+                            <div className="card border-0" shadow-sm rounded-3>
+                                <div className="card-body post-content">
+                                    <h4 className="text-normal">
+                                        {post.title}
+                                    </h4>
+                                    <div className="author mt-3">
+                                        <span>
+                                            <i className="fa fa-user"></i> {post.user.name}
+                                        </span>
+                                        <span>
+                                            <i className="fa fa-folder ms-4 ml-4"></i> {" "} {post.category.name}
+                                        </span>
+                                        <span>
+                                            <i className="fa fa-calendar ms-4 ml-4"></i> {" "} {DateID(new DataTransfer(post.created_at))}
+                                        </span>
+                                    </div>
+                                    <hr />
+                                    <img 
+                                        src={post.image}
+                                        class="rounded-3 w-100 mb-3"
+                                        alt={post.title}
+                                    />
+                                    <p dangerouslySetInnerHTML={{  __html: post.content }}></p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className="col-md-4">
+                        <h5 className="text-uppercase">
+                            <i className="fa fa-book"></i> BERITA TERBARU
+                        </h5>
+                        <hr />
+                        {loadingPost ? (
+                            <Loading />
+                        )}
+                    </div>
+                </div> {
+                    
+                }
+            </div>
         </LayoutWeb>
     )
 }
