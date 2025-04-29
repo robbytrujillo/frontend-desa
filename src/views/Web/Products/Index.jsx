642456a50e399ded4a476eb207fc6dc176bc0613
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-29 14:38:13 +0700

    Desa Aplication | Update View Products Web Index

--- src/views/Web/Products/Index.jsx
+++ src/views/Web/Products/Index.jsx
@@ -4,6 +4,12 @@ import React, { useState, useEffect } from "react";
 // import layout
 import LayoutWeb from "../../../layouts/Web";
 
+// import service api
+import Api from "../../../services/Api";
+
+// import component alert
+import AlertDataEmpty from "../../../components/general/AlertDataEmpty";
+
 export default function WebProductsIndex() {
     return (
         <LayoutWeb>
