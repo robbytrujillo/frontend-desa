7c200b5849014b6a71407a5f32aa8fbc1fab0b6d
robbytrujillo <robbyikusuma88@gmail.com>
2025-04-30 11:04:43 +0700

    Desa Aplication | Create View Product Web Show

---
+++ src/views/Web/Products/Show.jsx
@@ -0,0 +1,10 @@
+// import layout
+import LayoutWeb from "../../../layouts/Web";
+
+export default function WebProductsShow() {
+    return (
+        <LayoutWeb>
+            <h1>Halaman Products Show</h1>
+        </LayoutWeb>
+    )
+}
